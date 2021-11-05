cbc595ec464f74910844897cbf50d90da71dfdf5
enewcombe30 <enewcombe30@gmail.com>
2021-11-05 13:31:51 +0000

    first php script upload

---
+++ Static/Php/contactform.php
@@ -0,0 +1,17 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $mailFrom = $_POST['mail'];
+    $message = $_POST['messsage'];
+
+    $mailto = "enewcombe30@gmailcom";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have recieved an e-mail from ".$name.".\n\n".$message;
+
+    mail($mailto, $subject, $txt, $header);
+    header("Location: index.php?mailsend");
+
+}
+?>
