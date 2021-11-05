3408faa8312f08ffcbf9182795d83bb074a6658b
enewcombe30 <enewcombe30@gmail.com>
2021-11-05 14:03:25 +0000

    Updated email

--- Static/Php/contactform.php
+++ Static/Php/contactform.php
@@ -6,7 +6,7 @@ if (isset($_POST['submit'])) {
     $mailFrom = $_POST['mail'];
     $message = $_POST['messsage'];
 
-    $mailto = "enewcombe30@gmailcom";
+    $mailto = "enewcombe30@outlook.com";
     $headers = "From: ".$mailFrom;
     $txt = "You have recieved an e-mail from ".$name.".\n\n".$message;
 
